d1dc878610a26a5c4564046fde48dca5a83e042e
Rajeev Kumar <rajeevks21@iitk.ac.in>
2024-02-02 15:10:50 +0530

    Removed twice problem

--- server/users.js
+++ server/users.js
@@ -4,10 +4,11 @@ const addUser = ({ id, name, room }) => {
     name = name.trim().toLowerCase();
     room = room.trim().toLowerCase();
 
-    // const existingUser = users.find((user) => user.room == room && user.name == name);
-    // if(existingUser){
-    //     return { error: 'Username is taken' };
-    // }
+    const existingUser = users.find((user) => user.room == room && user.name == name);
+    if(existingUser){
+        return { error: 'Username is taken' };
+    }
+    
     const user = {id, name, room};
     users.push(user);
     return { user }
