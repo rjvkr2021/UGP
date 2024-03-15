e0754d1196e8477a898092b65357ef1bfaf5834b
Rajeev Kumar <rajeevks21@iitk.ac.in>
2024-03-15 16:33:29 +0530

    Changed database structure

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const cors = require("cors");
 
-const { addMessage } = require("./users");
+const { addMessage, editMessage } = require("./users");
 
 const router = require("./router");
 
@@ -30,7 +30,7 @@ io.on("connect", (socket) => {
 
 	socket.on("sendMessage", async ({ user_name, user_room, message_text }) => {
 		try {
-			const message_id = await addMessage({ socket_id: socket.id, user_name: user_name, user_room: user_room, message_text: message_text });
+			const message_id = await addMessage({ message_text: message_text });
 			io.to(user_room).emit("message", { message_id: message_id, message_text: message_text, message_sender: user_name });
 		} catch (error) {
 			console.error("Error:", error);
@@ -39,7 +39,7 @@ io.on("connect", (socket) => {
 
 	socket.on("editMessage", async ({ user_room, originalMessage, editedMessageText }) => {
 		try {
-			// const message_id = await addMessage({ socket_id: socket.id, user_name: user_name, user_room: user_room, message_text: message_text});
+			await editMessage({ message_id: originalMessage.message_id, editedMessageText: editedMessageText });
 			io.to(user_room).emit("editedMessage", { originalMessage, editedMessageText });
 		} catch (error) {
 			console.error("Error:", error);
@@ -51,6 +51,6 @@ io.on("connect", (socket) => {
 	})
 });
 
-server.listen(5000, () => {
+server.listen(5001, () => {
 	console.log('Server running on port 5001');
 });
--- server/users.js
+++ server/users.js
@@ -3,17 +3,14 @@ const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("database.db");
 
 db.run(`CREATE TABLE IF NOT EXISTS database (
-    socket_id TEXT,
-    user_name TEXT,
-    user_room TEXT,
     message_id INTEGER PRIMARY KEY AUTOINCREMENT,
     message_text TEXT
 )`);
 
-const addMessage = async ({ socket_id, user_name, user_room, message_text }) => {
+const addMessage = async ({ message_text }) => {
 	return new Promise((resolve, reject) => {
-		const query = `INSERT INTO database (socket_id, user_name, user_room, message_text) VALUES (?, ?, ?, ?)`;
-		db.run(query, [socket_id, user_name, user_room, message_text], function (err) {
+		const query = `INSERT INTO database (message_text) VALUES (?)`;
+		db.run(query, [message_text], function (err) {
 			if (err) {
 				reject(err);
 			} else {
@@ -23,4 +20,17 @@ const addMessage = async ({ socket_id, user_name, user_room, message_text }) => 
 	});
 };
 
-module.exports = { addMessage };
+const editMessage = async ({ message_id, editedMessageText }) => {
+	return new Promise((resolve, reject) => {
+		const query = `UPDATE database SET message_text = ? WHERE message_id = ?`;
+		db.run(query, [editedMessageText, message_id], function (err) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(this.changes);
+			}
+		});
+	});
+};
+
+module.exports = { addMessage, editMessage };
